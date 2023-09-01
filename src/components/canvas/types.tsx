411798668e269405e7594a2cbbae412e15459ba0
seankwarren <seankwarren722@gmail.com>
2023-08-31 20:46:10 -0700

    docs: jsdocs

--- src/components/canvas/types.tsx
+++ src/components/canvas/types.tsx
@@ -9,14 +9,36 @@ type ActionFunction =
     | ((state: CanvasState) => void);
 
 export type ActionType = {
+    /** Tooltip text for the action. */
     title: string;
+    /** The name of the lucide icon to be used. */
     icon: IconName;
-    onClick: ActionFunction
+    /** Function to call when the action is clicked. */
+    onClick: ActionFunction;
+    /** Array of node types where this action is allowed. */
     allowedNodeTypes: NodeTypes[];
+    /** Array of edge types where this action is allowed. */
     allowedEdgeTypes: EdgeTypes[];
+    /** Indicates if this action is an edit action. */
+    isEditAction: boolean;
+    /**
+     * Specifies if the action can be used with a mixed selection
+     * of elements (i.e., nodes and edges).
+     */
     allowsMixedElements: boolean;
+    /**
+     * Specifies if the action can be used when multiple elements are selected.
+     */
     allowsMultiSelection: boolean;
+    /**
+     * Specifies if the action requires multiple elements to be selected
+     * for it to be available.
+     */
     requiresMultiSelection: boolean;
+    /**
+     * Specifies if the action requires a single type of element to be selected
+     * (i.e., all elements must be of type 'card').
+     */
     requiresSingleType: boolean;
 };
 
